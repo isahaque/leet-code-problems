b0db27f639d9f4e23ad0d92ff38b8577904ad2d1
Isahaque Miah <isahakbd@outlook.com>
2023-10-23 16:27:16 +0600

    merge sorted array

---
+++ Leetcode-problem-study/Leetcode-problem-study/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Leetcode_problem_study;
+
+
+var result=MergeSortedArray.Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2,5,6 }, 3);
+
+Console.ReadLine();
