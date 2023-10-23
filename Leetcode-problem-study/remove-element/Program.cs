750b49f93ef8735e487e31b40f784f1d92134860
Isahaque Miah <isahakbd@outlook.com>
2023-10-23 16:46:30 +0600

    remove element problem

---
+++ Leetcode-problem-study/remove-element/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using remove_element;
+
+
+
+var index = RemoveElement.GetRemoveableIndex(new int[] { 3, 2, 2, 3 }, 3);
+Console.WriteLine(index);
---
+++ Leetcode-problem-study/remove-element/RemoveElement.cs
@@ -0,0 +1,20 @@
+﻿namespace remove_element
+{
+    public static class RemoveElement
+    {
+        public static int GetRemoveableIndex(int[] nums,int val)
+        {
+            int index = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[index] = nums[i];
+                    index++;
+                }
+            }
+
+            return index;
+        }
+    }
+}
