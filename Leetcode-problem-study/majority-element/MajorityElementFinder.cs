5e29d4392124b1acc4d6122441003f4449650514
Isahaque Miah <isahakbd@outlook.com>
2023-10-25 13:48:08 +0600

    Find Majority Element

---
+++ Leetcode-problem-study/majority-element/MajorityElementFinder.cs
@@ -0,0 +1,40 @@
+﻿namespace majority_element
+{
+
+    /*
+     Example 1:
+
+    Input: nums = [3,2,3]
+    Output: 3
+    Example 2:
+
+    Input: nums = [2,2,1,1,1,2,2]
+    Output: 2
+     */
+    public static class MajorityElementFinder
+    {
+
+        public static int Find(int[] nums)
+        {
+            int major = nums[0];
+            int count = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if(count==0)
+                {
+                    major = nums[i];
+                    count = 1;
+                }
+                else if (major == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            return major;
+        }
+    }
+}
---
+++ Leetcode-problem-study/majority-element/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using majority_element;
+
+var element = MajorityElementFinder.Find(new int[] { 2,2,1,1,1,2,2 });
+Console.WriteLine(element);
